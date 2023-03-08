905d1ba8bcf9387f3df922177c4cd77541ac9a58
Victorino Gomes <victorinogomes1@gmail.com>
2023-03-08 17:13:05 +0000

    Update genpass.js

--- API/genpass.js
+++ API/genpass.js
@@ -2,7 +2,7 @@ fetch('http://localhost:5000/sendcode', {
     method: "GET",
     headers: {
         'Content-type': 'application/json',
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidmljdG9yaW5vIiwiaWQiOiI2Mjc3YzlhOGQ5ZDU2NTQ0Yzg5Y2I5ZTMiLCJpYXQiOjE2NTM0OTg2NTV9.VqnYZSKG2o1__dgPkV_soNZD2Ui_dHgUYUMrsAXa83k",// Here you can add your token
+        "Authorization": // Here you can add your token
     },
 }
 ).then(function (response) {
