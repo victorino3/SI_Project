3d1a63db18c9d70cd4601c110cbd3d3f203908f9
Victorino Gomes <victorinogomes1@gmail.com>
2023-04-24 01:17:59 +0100

    Update get-token.js

--- SIProject/auth/get-token.js
+++ SIProject/auth/get-token.js
@@ -7,5 +7,3 @@ const GetToken = (req) => {
 }
 module.exports = GetToken
 
-
-// BEREAR eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidmljdG9yaW5vIiwiaWQiOiI2Mjc3YzlhOGQ5ZDU2NTQ0Yzg5Y2I5ZTMiLCJpYXQiOjE2NTQ5OTM4MjJ9.trZ3I7OVUNAGio548bA30bhZvbAQMx4RGzS9R8RzgGY
